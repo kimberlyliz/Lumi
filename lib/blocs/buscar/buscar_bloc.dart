75e91fe7a48bcf6f0300456dca5b2d476fc080f3
kimberlyliz <101994019+kimberlyliz@users.noreply.github.com>
2022-11-22 14:12:24 -0300

    bloc folder

--- lib/blocs/buscar/buscar_bloc.dart
+++ lib/blocs/buscar/buscar_bloc.dart
@@ -1,15 +1,15 @@
 /* Toda a lógica de negócios fica dentro do arquivo BLoC. 
 Ele simplesmente aceita eventos, executa a lógica e fornece os estados. */
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
-part 'buscar_event.dart';
-part 'buscar_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lumi_code/blocs/buscar/buscar_event.dart';
+import 'package:lumi_code/blocs/buscar/buscar_state.dart';
 
 class BuscarBloc extends Bloc<BuscarEvent, BuscarState> {
-  BuscarBloc() : super(BuscarInitial()) {
-    on<BuscarEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  BuscarBloc(BuscarState initialState) : super(BuscarLoadingState());
+  @override
+  Stream<BuscarState> mapEventToState(BuscarEvent event) async* {
+    throw UnimplementedError();
   }
+  
 }
--- lib/blocs/buscar/buscar_event.dart
+++ lib/blocs/buscar/buscar_event.dart
@@ -1,11 +1,19 @@
 /*Os eventos dizem ao BLoC para fazer algo. Um evento pode ser disparado de qualquer lugar, 
 como de um widget de IU.*/
 
-/*1) Load Page 
+/*1) Carregar a lista  
   2) Search container - Load Searches (Mudar do State 1 para o 2)
  */
 
-part of 'buscar_bloc.dart';
+import 'package:equatable/equatable.dart';
 
-@immutable
-abstract class BuscarEvent {}
+class BuscarEvent extends Equatable{
+  @override
+  List<Object> get props => [];
+}
+
+class BuscarFechtList extends BuscarEvent {}
+
+class BuscarFechtWithEmptyList extends BuscarEvent {}
+
+
