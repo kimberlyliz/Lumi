de310bf523e165d4a676a65965a74d0b5f8dae36
kimberlyliz <101994019+kimberlyliz@users.noreply.github.com>
2022-11-22 11:15:02 -0300

    bloc folder

--- lib/blocs/descricao/descricao_event.dart
+++ lib/blocs/descricao/descricao_event.dart
@@ -1,3 +1,19 @@
+/*1) Load Page (Mudar do State 1 para o 2)
+
+2) Add (Adicionar o filme/série para a lista individual) 
+
+3) Rate (Avaliar o filme/série com estrelas entre 0 e 5)
+
+4) Share (Compartilhar o filme/série com outros usuários)
+
+5) Redirect (Direciona o usuário para algum site, como Netflix ou Globoplay, para que ele possa ver o filme)
+
+6) Play ( Da play no trailer dentro do próprio app)
+
+7) Comment ( Direcionar o usuário para a barra de comentários)
+ */
+
+
 part of 'descricao_bloc.dart';
 
 abstract class DescricaoEvent extends Equatable {
--- lib/blocs/descricao/descricao_state.dart
+++ lib/blocs/descricao/descricao_state.dart
@@ -1,3 +1,7 @@
+/*1) Page Inital (Inicializar o bloc) 
+
+2) Page loaded ( Mostrar a página carregada) */
+
 part of 'descricao_bloc.dart';
 
 abstract class DescricaoState extends Equatable {
